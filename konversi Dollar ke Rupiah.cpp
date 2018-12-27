289bc99f7d4ef3a168fdef3c6040e790f2cda0f1
MUTIARA IZMI NURFADLILAH <45525577+MUTIARAIZMI@users.noreply.github.com>
2018-12-27 12:04:35 +0700

    Update konversi Dollar ke Rupiah.cpp

--- konversi Dollar ke Rupiah.cpp
+++ konversi Dollar ke Rupiah.cpp
@@ -4,7 +4,7 @@ int main()
 {
     long rupiah=15100,dollar,kurs;
     printf("PROGRAM KONVERSI DOLLAR KE RUPIAH\n");
-    printf("Masukkan nilai Dollar $USD =");scanf("%",&dollar);
+    printf("Masukkan nilai Dollar $USD =");scanf("%i",&dollar);
     kurs=rupiah*dollar;
     printf("kurs = %i * %i = Rp %i",dollar,rupiah,kurs);
     getch();
